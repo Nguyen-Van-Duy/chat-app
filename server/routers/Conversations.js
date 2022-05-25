f2b6b5f06633b0a30e06e313081e9a289136fa68
Nguyen-Van-Duy <nam1561999@gmail.com>
2022-05-26 00:07:28 +0700

    update file Conversation

--- server/routers/Conversations.js
+++ server/routers/Conversations.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import Conversation from '../models/Conversation';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/id', async (req, res) => {
+    const newConversation = new Conversation({
+        members: [req.body.serderId, req.body.receiveId]
+    })
     res.json({
         status: 200,
         message: 'Sucsess'
     })
+    try {
+        const savedConversation = await newConversation.save()
+        res.status(200).json(savedConversation)
+    } catch (error) {
+        res.status(500).json({ error})
+    }
 })
 
 export default router;
